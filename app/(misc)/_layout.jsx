f3e72ff5f1b32aa8e2ab43a61d41f6d0934b5538
Kenneth sunjaya <kennethsunjaya@gmail.com>
2024-05-16 22:55:06 +0700

    Hide header on changepassword page

--- app/(misc)/_layout.jsx
+++ app/(misc)/_layout.jsx
@@ -8,6 +8,7 @@ const MiscLayout = () => {
     <>
       <Stack>
         <Stack.Screen name="addbank" options={{ headerShown: false }} />
+        <Stack.Screen name="changepassword" options={{ headerShown: false }} />
       </Stack>
       <StatusBar backgroundColor="rgba(238, 245, 255, 1)" style="dark" />
     </>
