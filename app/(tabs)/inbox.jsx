6be2c1c77f95adf4467451ea41ec6adfc24b5f19
Admantix <williamwijaya499@gmail.com>
2024-05-18 23:29:37 +0700

    Inbox Done

--- app/(tabs)/inbox.jsx
+++ app/(tabs)/inbox.jsx
@@ -1,11 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useUser } from "../hooks/Context";
+import { CustomCardInbox } from "../../components/CustomCard";
 
 const Inbox = () => {
+  const { userData } = useUser();
+
+  const data = userData.inbox;
   return (
-    <View>
-      <Text> Inbox</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full flex-1">
+      <View className="flex-row items-center justify-center p-5">
+        <Text className="text-black font-bold text-3xl">Inbox</Text>
+      </View>
+      <View className="flex-1 mx-5">
+        <View className="flex-row my-[3vh] flex-1">
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <CustomCardInbox
+                className="rounded-full w-[12.5vh] h-[20vh] bg-white mr-[2.5vh] mt-[3vh] flex justify-end"
+                bank={item.bank}
+                norek={item.norek}
+                title={item.title}
+              />
+            )}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- components/CustomCard.jsx
+++ components/CustomCard.jsx
@@ -1,94 +1,139 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
 import { icons } from "../constants";
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const CustomCardHome = (props) => {
-
   if (props.title !== "") {
     return (
-      <TouchableOpacity 
+      <TouchableOpacity
         className="min-h-[20vh] min-w-[12.5vh] bg-white rounded-[20px] mr-[2.5vh] mt-[3vh] flex justify-end"
         onPress={props.handlePress}
-        activeOpacity={0.7}>
+        activeOpacity={0.7}
+      >
         <View className="pb-[3vh] items-center">
-          <Image 
+          <Image
             source={props.logo}
             className="w-[8vh] h-[8vh]"
             resizeMode="contain"
           />
         </View>
         <View className="pb-[2vh]">
-          <Text className="font-pregular text-gray-200 text-sm text-center">{props.title}</Text>
-          <Text className="font-pregular text-gray-200 text-[10px] text-center">{props.subtitle}</Text>
+          <Text className="font-pregular text-gray-200 text-sm text-center">
+            {props.title}
+          </Text>
+          <Text className="font-pregular text-gray-200 text-[10px] text-center">
+            {props.subtitle}
+          </Text>
         </View>
       </TouchableOpacity>
-    )
-  }
-  else {
+    );
+  } else {
     return (
-      <TouchableOpacity 
+      <TouchableOpacity
         className="h-[20vh] w-[12.5vh] bg-white rounded-[20px] mt-[3vh] flex justify-end"
         onPress={props.handlePress}
-        activeOpacity={0.7}>
+        activeOpacity={0.7}
+      >
         <View className="pb-[4vh]">
-          <Text className="font-pregular text-gray-200 text-sm text-center">Tambah</Text>
-          <Text className="font-pregular text-gray-200 text-sm text-center">bank lain</Text>
+          <Text className="font-pregular text-gray-200 text-sm text-center">
+            Tambah
+          </Text>
+          <Text className="font-pregular text-gray-200 text-sm text-center">
+            bank lain
+          </Text>
         </View>
         <View className="pb-[3vh] items-center">
-          <Image 
+          <Image
             source={icons.plus}
             className="w-[6vh] h-[6vh]"
             resizeMode="contain"
           />
         </View>
       </TouchableOpacity>
-    )
+    );
   }
-  
-}
+};
 
 const CustomCardHistory = (props) => {
   return (
     <View className="bg-white rounded-[20px] mb-[3vh] flex p-5">
       <View>
-        <Text className="font-psemibold text-gray-200 text-sm mb-3">{props.date}</Text>
-        <Text className="font-pregular text-gray-200 text-s">{props.title}</Text>
+        <Text className="font-psemibold text-gray-200 text-sm mb-3">
+          {props.date}
+        </Text>
+        <Text className="font-pregular text-gray-200 text-s">
+          {props.title}
+        </Text>
       </View>
       <SafeAreaView className="items-end flex-row">
         <Text className="text-gray-200 text-s">{props.subtitle}</Text>
         <View className="ml-auto">
-        <Image 
-          source={props.logo}
-          className="w-[6vh] h-[6vh]"
-          resizeMode="contain"
-        />
+          <Image
+            source={props.logo}
+            className="w-[6vh] h-[6vh]"
+            resizeMode="contain"
+          />
         </View>
-        
       </SafeAreaView>
-      
     </View>
-  )
-}
+  );
+};
 
 const CustomCardRekening = (props) => {
   return (
-    <TouchableOpacity className="bg-white rounded-[20px] mb-[3vh] flex p-5" onPress={props.handlePress} activeOpacity={0.7}>
+    <TouchableOpacity
+      className="bg-white rounded-[20px] mb-[3vh] flex p-5"
+      onPress={props.handlePress}
+      activeOpacity={0.7}
+    >
       <View className="items-end flex-row">
         <View>
-          <Text className="font-psemibold text-gray-200 text-sm mb-3">Rekening {props.namabank}</Text>
+          <Text className="font-psemibold text-gray-200 text-sm mb-3">
+            Rekening {props.namabank}
+          </Text>
           <Text className="text-gray-200 text-xl">{props.norek}</Text>
         </View>
         <View className="ml-auto">
-        <Image 
-          src={props.logo}
-          className="w-[6vh] h-[6vh]"
-          resizeMode="contain"
-        />
+          <Image
+            src={props.logo}
+            className="w-[6vh] h-[6vh]"
+            resizeMode="contain"
+          />
         </View>
       </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export { CustomCardHome, CustomCardHistory, CustomCardRekening }
+const CustomCardInbox = (props) => {
+  return (
+    <TouchableOpacity
+      className="bg-white rounded-[20px] mb-[3vh] flex p-5"
+      onPress={props.handlePress}
+      activeOpacity={0.7}
+    >
+      <View className="items-end flex-row">
+        <View>
+          <Text className="font-psemibold text-gray-200 text-lg mb-3">
+            Rekening {props.bank}
+          </Text>
+          <Text className="text-gray-200 text-sm font-psemibold">
+            {props.norek}
+          </Text>
+          <Text className="text-gray-200 text-sm font-psemibold">
+            {props.title}
+          </Text>
+        </View>
+        <View className="ml-auto"></View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export {
+  CustomCardHome,
+  CustomCardHistory,
+  CustomCardRekening,
+  CustomCardInbox,
+};
